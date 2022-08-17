b7f625d0ab480e0a4789ba891f3d553eb17e84dd
Jamal Javed <>
2022-08-17 15:36:13 +0400

    First Commit

--- product-service/src/main/java/com/service/product/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.service.product.service;
-
-import com.service.product.payload.ProductDetails;
-
-public interface ProductService {
-
-    ProductDetails getProductByProductId(String productId);
-
-}
