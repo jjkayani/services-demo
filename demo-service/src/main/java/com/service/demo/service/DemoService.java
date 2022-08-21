4f677f1ed333d1d94ae4fe75086ca1f5ba873c1e
Jamal Javed <>
2022-08-21 14:55:27 +0400

    Revert "Second Commit"
    This reverts commit 6827b616d728e9b87c272849f1dd413ac5e990f9.

--- demo-service/src/main/java/com/service/demo/service/DemoService.java
+++
@@ -1,9 +0,0 @@
-package com.service.demo.service;
-
-import com.service.demo.payload.RequestPayload;
-import com.service.demo.payload.ResponsePayload;
-
-public interface DemoService {
-
-    ResponsePayload findPerfectCycle(RequestPayload requestPayload);
-}
